e95eab503f90a75aec2ed625f8465aaaebd248d0
George Borsato <georginhoborsato@gmail.com>
2021-04-01 18:37:07 -0300

    refactor task controller

--- api-teste/app/controllers/tasks_controller.rb
+++ api-teste/app/controllers/tasks_controller.rb
@@ -1,15 +1,16 @@
 class TasksController < ApplicationController
 
     def index
-        tasks = Task.all
-
+        #tasks = Task.all
+        tasks = ServiceTask.get_all_tasks
         if tasks
             render json: tasks, status: :ok
         end
     end
 
     def show
-        task = Task.find_by(id: params[:id])
+        #task = Task.find_by(id: params[:id])
+        task = ServiceTask.get_task_pk(params[:id])
 
         if task
             render json: task, status: :ok
@@ -19,11 +20,10 @@ class TasksController < ApplicationController
     end
 
     def store
-        task = Task.new(tasks_params)
+        task = ServiceTask.new(tasks_params).create_task
 
-        if task.save
+        if task
             render json: task, status: :created
-
         else
             render json: {message: 'Error in create task'}, status: :unprocessable_entity
         end
@@ -31,10 +31,14 @@ class TasksController < ApplicationController
 
 
     def update
-        task = Task.find_by(id: params[:id])
+        #task = Task.find_by(id: params[:id])
+
+
+        task = ServiceTask.update_task(id: params[:id], task: tasks_params)
+
 
         if task
-            task.update(tasks_params)
+            #task.update(tasks_params)
             render json: task, status: :ok
         else
             render json: {message: 'Task not found'}, status: :not_found
@@ -42,14 +46,16 @@ class TasksController < ApplicationController
     end
 
     def destroy
-        task = Task.find_by(id: params[:id])
+        #task = Task.find_by(id: params[:id])
+
+        task = ServiceTask.delete_task(params[:id])
 
         if task
-            task.destroy
+            #task.destroy
             render json: {message: "Task deleted with success"}, status: :ok
 
         else
-            render json: {message: "Project not found"}, status: :not_found
+            render json: {message: "Task not found"}, status: :not_found
         end
     end
 
